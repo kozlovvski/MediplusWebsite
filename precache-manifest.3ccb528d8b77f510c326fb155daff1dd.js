fe0b97172ddde420fc644b1153696bd312bc63ce
kozlovvski <mcbiggos@gmail.com>
2019-06-28 01:15:27 +0200

    Updates

---
+++ precache-manifest.3ccb528d8b77f510c326fb155daff1dd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5aeaceffa9d9c81ae3c992031948715",
+    "url": "/MediplusWebsite/index.html"
+  },
+  {
+    "revision": "96c2a87be7f1890c2e78",
+    "url": "/MediplusWebsite/static/css/main.69b9e857.chunk.css"
+  },
+  {
+    "revision": "e747ff14ece38cf2d717",
+    "url": "/MediplusWebsite/static/js/2.bc6d40e6.chunk.js"
+  },
+  {
+    "revision": "96c2a87be7f1890c2e78",
+    "url": "/MediplusWebsite/static/js/main.d40fdfc0.chunk.js"
+  },
+  {
+    "revision": "b0c993285882c55f27a7",
+    "url": "/MediplusWebsite/static/js/runtime~main.ff30a35b.js"
+  },
+  {
+    "revision": "4f832ebe9ed73c705fc233ef39cef0ef",
+    "url": "/MediplusWebsite/static/media/Flaticon.4f832ebe.ttf"
+  },
+  {
+    "revision": "522c145904d2f954397967493365b848",
+    "url": "/MediplusWebsite/static/media/Flaticon.522c1459.woff2"
+  },
+  {
+    "revision": "56a1ed7081f953e224e1fc4a4e6bf2db",
+    "url": "/MediplusWebsite/static/media/Flaticon.56a1ed70.svg"
+  },
+  {
+    "revision": "80477fcaabc8b4535fc232997ff0b615",
+    "url": "/MediplusWebsite/static/media/Flaticon.80477fca.woff"
+  },
+  {
+    "revision": "df8bad8c83de2123358495c115873198",
+    "url": "/MediplusWebsite/static/media/Flaticon.df8bad8c.eot"
+  },
+  {
+    "revision": "6d9648cb9214bb9d3e8778b172c9e275",
+    "url": "/MediplusWebsite/static/media/about.6d9648cb.jpg"
+  },
+  {
+    "revision": "bc761fc91cf845a2cde67a215ae7efbb",
+    "url": "/MediplusWebsite/static/media/bg_3.bc761fc9.jpg"
+  }
+]);
