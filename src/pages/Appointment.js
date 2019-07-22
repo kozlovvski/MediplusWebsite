6db6c27f77752d36ccc522f0cf7bd996a7658318
kozlovvski <mcbiggos@gmail.com>
2019-07-22 20:49:44 +0200

    remove content for deployment

--- src/pages/Appointment.js
+++ src/pages/Appointment.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 
 import Hero from "components/Hero";
 
@@ -8,7 +7,6 @@ export default function Appointment() {
 	document.title = "Appointment || Mediplus";
 
 	return (
-		<React.Fragment>
 			<Hero
 				subheading="Make an appointment"
 				title="How can we help you?"
@@ -17,22 +15,5 @@ export default function Appointment() {
 				backgroundImage={require("images/bg_1.jpg")}
 				hideAppointmentButton
 			/>
-			<section id="Appointment">
-				<Container className="py-3 py-md-5">
-					<Row className="justify-content-center mb-2">
-						<Col md={8} lg={6} className="text-center">
-							<h2 className="mb-2">Our Doctors</h2>
-							<p className="mb-2">
-								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
-								asperiores adipisci dolorem voluptatum.
-							</p>
-						</Col>
-					</Row>
-					<Row>
-						<h2>Add something here...</h2>
-					</Row>
-				</Container>
-			</section>
-		</React.Fragment>
 	);
 }
